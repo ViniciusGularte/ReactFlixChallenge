d7409f92f5c7389be31faf401e268013ba362ff0
Vinicius Martin <viniciusgulartemartin@gmail.com>
2019-02-23 22:14:18 -0300

    Add Firebase global

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import App from './app'
 import configureStore from './redux-flow/configure-store'
-
+import { db } from './config/firebase'
 const store = configureStore()
 
-const db = firebase.database()
 const videos = db.ref('videos')
+
 
 videos.on('value',(snapshot) =>{
   console.log(snapshot.val())
-}.(error) => {
+},(error) => {
   console.log('error:',error)
 })
 
