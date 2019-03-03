e4098f690e6f516b22ec98a00e6a5215b731d7bb
Vinicius Martin <viniciusgulartemartin@gmail.com>
2019-03-03 18:13:10 -0300

    Add clean form after insert

--- src/components/register-video.js
+++ src/components/register-video.js
@@ -19,13 +19,16 @@ const RegisterVideo = ({onSubmit}) =>(
   </form>
 )
 const mapDispatchToProps = (dispatch) => ({
-  onSubmit: (e) => {
+  onSubmit: async (e) => {
     e.preventDefault()
+    e.persist()
     const {
       id:{ value: id },
       title:{ value: title }
     } = e.target
-    dispatch(registerVideo({ id, title }))
+    e.target.reset()
+    e.target[0].focus()
+    await dispatch(registerVideo({ id, title }))
 
   }
 })
